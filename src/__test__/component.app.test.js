69eca74c94f8dd0cfeebdb37db5852ffba2f5abe
Jose Collas <joseherminiocollas@goatstone.com>
2019-01-14 13:59:44 -0800

    update per lint

--- src/__test__/component.app.test.js
+++ src/__test__/component.app.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
 import App from '../App'
 
 it('matches a former snapshot', () => {
   const component = renderer.create(<App />)
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
