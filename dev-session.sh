96b6dc200685e6ffd8f2295125bb066f61fcba75
Jose Collas <joseherminiocollas@goatstone.com>
2019-01-22 13:40:25 -0800

    add script to open dev session

---
+++ dev-session.sh
@@ -0,0 +1,7 @@
+# gnome-terminal --tab -e "command" --tab --tab
+
+code .
+
+gnome-terminal --tab -e "yarn start"
+gnome-terminal --tab -e "yarn test:watch"
+gnome-terminal --tab -e "yarn lint:watch"
