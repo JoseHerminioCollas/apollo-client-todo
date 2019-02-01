c7f6fa67e47c8af9a7b59466a8a82aa146662cb5
Jose Collas <joseherminiocollas@goatstone.com>
2019-01-31 17:26:57 -0800

    add build files

---
+++ build/precache-manifest.6b45b82979d9f19c78121f32318b8ac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c35cd7e614e73448f96",
+    "url": "/static/js/main.3c35cd7e.chunk.js"
+  },
+  {
+    "revision": "a765ccc6bf542904a9dd",
+    "url": "/static/js/1.a765ccc6.chunk.js"
+  },
+  {
+    "revision": "3c35cd7e614e73448f96",
+    "url": "/static/css/main.e7e1aaf4.chunk.css"
+  },
+  {
+    "revision": "08817f1fd5eb37a51abf6cc3980ea7c3",
+    "url": "/index.html"
+  }
+];
